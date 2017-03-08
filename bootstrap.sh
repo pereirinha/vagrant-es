b608edd365a3b3ef6f0703dc1c0bf91155c7f0b5
Marco Pereirinha <marcopereirinha@MacBook-Pro-de-Marco.local>
2017-03-08 23:41:31 +0000

    Use ES 5.2.2

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,8 @@ sudo update-alternatives --config java
 sudo update-alternatives --config javac
 
 # install ES
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.0.1.deb
-sudo dpkg -i elasticsearch-5.0.1.deb
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.2.2.deb
+sudo dpkg -i elasticsearch-5.2.2.deb
 rm elasticsearch*
 
 # Allow ES to be accessed by host
