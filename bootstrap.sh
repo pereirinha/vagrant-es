0d7c1d939df7b6b5b767b47d77b4a0fb65846e82
Marco Pereirinha <marcopereirinha@MacBook-Pro-de-Marco.local>
2017-03-08 22:36:16 +0000

    ES version 2.2.1

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
-# install openjdk-8
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk
-sudo update-alternatives --config java
-sudo update-alternatives --config javac
+
+# install openjdk-7
+sudo apt-get install -y openjdk-7-jre-headless
 
 # install ES
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.0.1.deb
-sudo dpkg -i elasticsearch-5.0.1.deb
+wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.2.1/elasticsearch-2.2.1.deb
+sudo dpkg -i elasticsearch-2.2.1.deb
 rm elasticsearch*
 
 # Allow ES to be accessed by host
 sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
 
+# Start ES
 sudo /etc/init.d/elasticsearch restart
