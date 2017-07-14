382fa9762f5a0d0f0d1b527acd84bbc641af0890
Marco Pereirinha <marco@10up.com>
2017-07-14 16:02:16 +0100

    Update to ES 5.3.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,8 @@ sudo update-alternatives --config java
 sudo update-alternatives --config javac
 
 # install ES
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.2.2.deb
-sudo dpkg -i elasticsearch-5.2.2.deb
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.3.0.deb
+sudo dpkg -i elasticsearch-5.3.0.deb
 rm elasticsearch*
 
 # Allow ES to be accessed by host
